3e7c909bf9c76889adec943cdcf231844b0b165c
MingTori <s090ldbread@gmail.com>
2025-03-31 17:12:05 +0900

    feat : 로그인

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls'))
+    path('user/', include('user.urls')),
+    path('', home_view),
 ]
--- myproject/user/forms.py
+++ myproject/user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
 
 class SignUpForm(UserCreationForm) :
@@ -8,3 +8,13 @@ class SignUpForm(UserCreationForm) :
     class Meta :
         model = CustomUser
         fields = ['nickname', 'email', 'username', 'password1', 'password2']
+
+class LoginForm(AuthenticationForm) :
+    username = forms.CharField(
+        label='아이디',
+        widget=forms.TextInput(attrs={'placeholder': '아이디를 입력하세요'})
+    )
+    password = forms.CharField(
+        label='비밀번호',
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 입력하세요'})
+    )
