3c2e272aae879691edff6d60123799064e435ab6
MingTori <s090ldbread@gmail.com>
2025-04-04 22:08:53 +0900

    feat: 게시글 상세보기 수정 삭제 기능추가

--- myproject/board/views.py
+++ myproject/board/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from .models import Post
-from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -14,7 +13,7 @@ def post_create(request):
             post = form.save(commit=False)
             post.author = request.user  
             post.save()
-            return redirect('/') 
+            return redirect('post_list') 
 
     else:
         form = PostForm()
@@ -34,3 +33,35 @@ def toggle_like(request, post_id):
         post.likes.add(request.user)
 
     return HttpResponseRedirect(reverse('post_list'))
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id) 
+    return render(request, 'board/post_detail.html', {'post': post})
+
+@login_required(login_url='/user/login/')
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:  
+        return redirect('post_detail', post_id=post.id)
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=post.id)  
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'board/post_edit.html', {'form': form})
+
+@login_required(login_url='/user/login/')
+def post_delete(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:  
+        return redirect('post_detail', post_id=post.id)
+    
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')  
+    
+    return render(request, 'board/post_delete.html', {'post': post})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user.views import home_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('', home_view),
     path('board/', include('board.urls')),
 ]
+
+if settings.DEBUG :
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
